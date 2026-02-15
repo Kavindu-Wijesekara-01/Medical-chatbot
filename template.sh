a5b624a46e0dd1b377fd7579eb4e40ca5610e1cf
Kavindu Wijesekara <tech.kavinduwijesekara@gmail.com>
2026-02-15 16:32:28 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating dir
+mkdir -p src
+mkdir -p research
+
+# creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch requirements.txt
+touch README.md
+
+
+echo "Directory and files created successfully!"
